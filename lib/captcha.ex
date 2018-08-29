a70f34f9e987b014ac952c2014d96edc4628ddb8
roylez <me@roylez.info>
2018-08-29 14:47:49 +1000

    be more restrictive about received messages

--- lib/captcha.ex
+++ lib/captcha.ex
@@ -7,15 +7,13 @@ defmodule Captcha do
 
   # allow customize receive timeout, default: 10_000
   def get(timeout \\ 1_000) do
-    Port.open({:spawn, Path.join(:code.priv_dir(:captcha), "captcha")}, [:binary])
+    port = Port.open({:spawn, Path.join(:code.priv_dir(:captcha), "captcha")}, [:binary])
 
     # Allow set receive timeout
     receive do
-      {_, {:data, data}} ->
+      {^port, {:data, data}} ->
         <<text::bytes-size(5), img::binary>> = data
         {:ok, text, img }
-
-      other -> other
     after timeout ->
       {:timeout}
     end
